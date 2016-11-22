7920efcd19b0a1af2db98ca32480b06c5c85be5c
Patrick Gao <patrickgao0922@gmail.com>
2016-11-22 11:42:02 +1100

    create dist/app.js

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var _express = require('express')
+var _express2 = _interopRequireDefault(_express)
+
+function _interopRequireDefault(obj) {
+    return obj && obj._esModule ? obj : {default: obj}
+}
